198e143631682858d911716f3b9c7cd570b29273
ttu <tomi.tuhkanen@iki.fi>
2017-02-24 00:04:14 +0200

    Use async and bluebird in caledarService

--- src/calendarService.js
+++ src/calendarService.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 const google = require('googleapis');
 const googleAuth = require('google-auth-library');
 
@@ -8,47 +9,35 @@ class CalendarService {
     }
 
     process(eventCount = 1) {
-        return new Promise((resolve, reject) => {
-            this.getOAuthClient((error, result) => {
-                if (error)
-                    reject(error);
-                else {
-                    this.getCurrentOrNextEvent(eventCount, result, (error, events) => {
-                        if (error)
-                            reject(error);
-                        else
-                            resolve(events);
-                    });
-                }
-            });
-        });
-    }
-
-    getOAuthClient(cb) {
-        fs.readFile('client_secret.json', (err, content) => {
-            if (err) {
-                cb('Error loading client secret file - ' + err);
-                return;
+        return new Promise(async (resolve, reject) => {
+            try {
+                const client = await this.getOAuthClient();
+                const events = await this.getCurrentOrNextEvent(eventCount, client);
+                resolve(events);
+            } catch (err) {
+                reject(err);
             }
-
-            fs.readFile('calendar-authToken.json', (err, token) => {
-                if (err) {
-                    cb('Create new auth token - ' + err);
-                } else {
-                    const credentials = JSON.parse(content);
-                    var clientSecret = credentials.installed.client_secret;
-                    var clientId = credentials.installed.client_id;
-                    var redirectUrl = credentials.installed.redirect_uris[0];
-                    var auth = new googleAuth();
-                    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-                    oauth2Client.credentials = JSON.parse(token);
-                    cb(null, oauth2Client);
-                }
-            });
         });
     }
 
-    getCurrentOrNextEvent(eventCount, auth, cb) {
+    async getOAuthClient() {
+        const secretPromise = fs.readFileAsync('client_secret2.json');
+        const tokenPromise = fs.readFileAsync('calendar-authToken.json');
+        const secret = await secretPromise;
+        const token = await tokenPromise;
+
+        const credentials = JSON.parse(secret);
+        const clientSecret = credentials.installed.client_secret;
+        const clientId = credentials.installed.client_id;
+        const redirectUrl = credentials.installed.redirect_uris[0];
+
+        const auth = new googleAuth();
+        const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+        oauth2Client.credentials = JSON.parse(token);
+        return oauth2Client;
+    }
+
+    async getCurrentOrNextEvent(eventCount, auth) {
         const promises = this.calendars.map(c => {
             const params = {
                 auth: auth,
@@ -79,12 +68,10 @@ class CalendarService {
             });
         }, this);
 
-        Promise.all(promises).then(results => {
-            const allInOne = results.reduce((p, [success, result]) => {
+        return await Promise.all(promises).then(results => {
+            return results.reduce((p, [success, result]) => {
                 return success ? p.concat(result) : p;
             }, []);
-
-            cb(null, allInOne);
         });
     }
 }
